d3753256d4845e5a182116a537f279139d288ad5
SKULSHADY <anushekprasal@gmail.com>
2022-10-11 13:14:31 +1000

    base: Double tap to trigger doze [1/2]
    Thanks to yank555-lu for the original commit YAOSP-NG/frameworks_base@d6ac164.
    Cleaned up and ported to Android 10 by SKULSHADY.
    
    Change-Id: I4154925a0c7de37f1dc84b58b280691cc44d14c9

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6056,6 +6056,12 @@ public final class Settings {
          * @hide
          */
         public static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
+        
+        /**
+         * Use doubletap as doze pulse triggers
+         * @hide
+         */
+        public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
